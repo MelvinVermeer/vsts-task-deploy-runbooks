b9bd79c0c3c1e7375735fb47180b3bd047012bc7
Melvin Vermeer <Melvin.Vermeer@wortell.nl>
2018-05-30 14:58:30 +0200

    final verison

---
+++ DeployRunbooks/Deploy-Runbooks.ps1
@@ -0,0 +1,20 @@
+﻿[CmdletBinding()]
+Param(
+    [String] [Parameter(Mandatory = $true)] $ConnectedServiceNameSelector,    
+    [String] [Parameter(Mandatory = $true)] $ConnectedServiceNameARM, 
+    [string] [Parameter(Mandatory = $true)] $ResourceGroupName,
+    [string] [Parameter(Mandatory = $true)] $AutomationAccount,
+    [string] [Parameter(Mandatory = $true)] $RunbookFiles
+)
+
+$RunbookFilesPath = [System.IO.Path]::Combine($env:SYSTEM_ARTIFACTSDIRECTORY, $env:BUILD_DEFINITIONNAME, "drop", $RunbookFiles)
+
+Write-Output "Starting runbook deployment to: $ResourceGroupName/$AutomationAccount"
+Write-Output "Project path: $RunbookFilesPath"
+
+Get-ChildItem $RunbookFilesPath -Recurse | where {$_.Extension -eq ".ps1"} | % {
+    Write-Output "Upload runbook file" $_.FullName
+    Import-AzureRMAutomationRunbook -Path $_.FullName -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccount -Type PowerShell -Published:$true -Force | out-null
+}
+
+Write-Output "Finished runbook deployment to: $ResourceGroupName/$AutomationAccount"
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# vsts-task-deploy-runbooks
+Before building or publishing read the [docs](https://docs.microsoft.com/en-us/vsts/extend/publish/overview).
+
+to aquire tooling type `npm install -g tfx-cli`
+
+to get personal access token the [ms docs](https://docs.microsoft.com/en-us/vsts/accounts/use-personal-access-tokens-to-authenticate?view=vsts)
+
+to publish execute
+```
+tfx extension publish --manifest-globs vss-extension.json --share-with <vsts account> --rev-version --token <your access token> 
+```
